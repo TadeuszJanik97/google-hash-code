c3da8f269471e1356622bbc4d9bbd2be3530ada0
Lukas Cerny <cerny.lukas.oto@gmail.com>
2017-02-23 20:13:04 +0000

    video class

---
+++ src/Video.java
@@ -0,0 +1,15 @@
+public class Video {
+
+    public int size;
+    public int id;
+
+    public Video(int size, int id) {
+        this.size = size;
+        this.id = id;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+}
