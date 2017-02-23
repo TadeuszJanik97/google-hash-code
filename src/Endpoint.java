bf9cb9195532dedafee10be91c887e36472816ca
conradkun <conrichu@gmail.com>
2017-02-23 20:33:02 +0000

    Endpoint Update
    Completion of class Endpoint.

--- src/Endpoint.java
+++ src/Endpoint.java
@@ -5,35 +5,29 @@ public class Endpoint {
     private int ID;
     private int d_latency;
     private ArrayList<Video> videos;
-    private ArrayList<int> requests;
-    private Hashmap<Video, int> hash_map;
+    private ArrayList<Integer> requests;
+    private HashMap<Video, Integer> hash_map;
 
-    public Endpoint (int ID, int d_latency, ArrayList<Video> videos, ArrayList<int> requests){
+    public Endpoint (int ID, int d_latency, ArrayList<Video> videos, ArrayList<Integer> requests){
         this.ID = ID;
         this.d_latency = d_latency;
         hash_map = createHashMap(videos, requests);
     }
 
-    private Hashmap<Video, int> createHashMap(ArrayList<Video> videos, ArrayList<int> requests){
+    private HashMap<Video, Integer> createHashMap(ArrayList<Video> videos, ArrayList<Integer> requests){
         int size = videos.size();
-        Hashmap<Video, int> hash_map = new HashMap(size);
+        HashMap<Video, Integer> hash_map = new HashMap(size);
         for (int i = 0; i < size; i++){
-
+            hash_map.put(videos.get(i), requests.get(i));
         }
+        return hash_map;
     }
 
-    public int getID (){
-        return ID;
-    }
+    public int getID (){ return ID;}
 
-    public int getLatency (){
-        return d_latency;
-    }
-    public static int getAmount (){
+    public int getLatency (){ return d_latency;}
 
-    }
+    public static boolean hasVideo (){ return this.hash_map.isEmpty();}
 
-    public
-
-    //method hasvideo. getamout(video) = requests
+    public static int getAmount (Video video){ return this.hash_map.get(video);}
 }
